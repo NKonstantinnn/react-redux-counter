de19c999d0b3f6a75a92028ea6401d1ea01c0be9
kostya007 <kostya.1998.nosarev@mail.ru>
2019-11-17 18:39:02 +0700

    Connect a counter component with redux

--- src/components/Counter/Counter.spec.js
+++ src/components/Counter/Counter.spec.js
@@ -1,12 +1,84 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import {Counter} from './index';
-import { italic } from 'ansi-colors';
+import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import ConnectedCounter, {Counter} from './index';
+import {inc, dec} from '../../actions';
+import reducer, {initialState} from '../../reducer';
+import {createStore} from 'redux';
 
 describe('<Counter />', () => {
-    it('should containt count property', () => {
-        const wrapper = shallow(<Counter count="test" />);
-        expect(wrapper.length).toEqual(1)
-        expect(wrapper.find('h2').prop('children')).toEqual('test');
+    let wrapper;
+    const count = 0;
+
+    beforeEach(() => {
+        wrapper = shallow(<Counter count={count} />);
     });
-})
+
+    it('should containt count property', () => {
+        expect(wrapper.find('h2').prop('children')).toEqual(count);
+    });
+
+    it('should contains button with id="inc-btn"', () => {
+        expect(wrapper.find('#inc-btn').type()).toEqual('button');
+    });
+      
+    it('should contains button with id="dec-btn"', () => {
+        expect(wrapper.find('#dec-btn').type()).toEqual('button');
+    });
+});
+
+describe('Connected <Counter /> with mockStore', () => {
+    const mockStore = configureStore();
+    let store, wrapper;
+
+    beforeEach(() => {
+        store = mockStore(initialState);
+        wrapper = mount(
+            <Provider store={store}>
+                <ConnectedCounter />
+            </Provider>
+        );
+    });
+
+    it('should assign containing of tag<h2> and initial state ', () => {
+        expect(wrapper.find('h2').prop('children')).toEqual(initialState);
+    });
+
+    it('should assign count property and initial state', () => {
+        expect(wrapper.find(Counter).prop('count')).toEqual(initialState);
+    });
+
+    it('should dispatch an increment action', () => {
+        store.dispatch(inc());
+        expect(store.getActions()[0]).toEqual({type: 'INC'});
+    });
+
+    it('should dispatch an decrement action', () => {
+        store.dispatch(dec());
+        expect(store.getActions()[0]).toEqual({type: 'DEC'});
+    });
+});
+
+describe('Connected <Counter> with actual store + reducer', () => {
+    let store, wrapper;
+
+    beforeEach(() => {
+        store = createStore(reducer);
+        wrapper = mount(
+            <Provider store={store}>
+                <ConnectedCounter />
+            </Provider>
+        );
+    });
+
+    it('should dispatch an increment action', () => {
+        wrapper.find('#inc-btn').simulate('click');
+        expect(wrapper.find(Counter).prop('count')).toEqual(initialState + 1);
+    });
+
+    it('should dispatch an decrement action', () => {
+        wrapper.find('#dec-btn').simulate('click');
+        expect(wrapper.find(Counter).prop('count')).toEqual(initialState - 1);
+    });
+});
--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {inc, dec} from '../../actions';
 
-export const Counter = ({count}) => {
+export const Counter = ({count, inc, dec}) => {
     return (
       <div className="jumbotron">
         <h2>{count}</h2>
-        <button className="btn btn-primary btn-lg">
+        <button 
+          id="dec-btn"
+          className="btn btn-primary btn-lg"
+          onClick={dec}>
             DEC
         </button>
-        <button className="btn btn-primary btn-lg">
+        <button 
+          id="inc-btn"
+          className="btn btn-primary btn-lg"
+          onClick={inc}>
             INC
         </button>
       </div>
     );
 };
+
+const mapStateToProps = (state) => {
+  return {
+    count: state
+  };
+};
+
+const mapDispatchToProps = {
+  inc, 
+  dec
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
